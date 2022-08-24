19cc8991d7a52820e73f47cadced219bf0985be5
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2022-08-23 21:24:06 -0500

    Add files via upload
    Updated through section 9 video 136.

--- shared.js
+++ shared.js
@@ -1,10 +1,16 @@
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let selectPlanButtons = document.querySelectorAll('.plan button');
+let modalNoButton = document.querySelector('.modal__action--negative');
 
 
 //console.dir(backdrop);
 // console.dir(selectPlanButtons);
+
+function closeModal() {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+}
 
 for(let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
@@ -12,3 +18,7 @@ for(let i = 0; i < selectPlanButtons.length; i++) {
     backdrop.style.display = 'block';
     });
 }
+
+backdrop.addEventListener('click', closeModal);
+modalNoButton.addEventListener('click', closeModal);
+
